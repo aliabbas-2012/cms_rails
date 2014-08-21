986c6f410e23db4206df6cf9dd06cfeebc4e3274
Ali Abbas <ali.abbas@nxb.com.pk>
2014-08-21 16:11:18 +0500

    push all code

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,20 @@
 class DemoController < ApplicationController
   def index
   end
+
+  def createUser
+
+    user = User.new
+    user.first_name = 'ali'
+    user.last_name = 'abbas'
+    if params['email'].nil?
+      user.email = 'itsgeniusstar@gmail.com'
+    else
+      user.email = params['email']
+    end
+    user.password = Digest::MD5.hexdigest('admin')
+    user.save
+
+    @errors =  user.errors.full_messages
+  end
 end
