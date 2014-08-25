089a652230f5990be5023d1f9c3f1fe87d4e31ea
Ali Abbas <itsgeniusstar@gmail.com>
2014-08-25 11:23:06 +0500

    post

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -23,7 +23,7 @@ class DemoController < ApplicationController
     last = Subject.last
 
     start = last.nil?? 1:last.id
-    puts start
+
     (start..start+4).each do |val|
       subject = Subject.create({:name=>"Maths #{val}",:description=>"Maths For It #{val}",:position=>val,:is_visible=>1})
       @subject[subject.id] = subject
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ActiveRecord::Base
   # attr_accessible :name, :permalink, :body, :position, :is_visible
+
+  before_create :set_postion
+
+  private
+  def set_postion
+    last = Post.last
+    self.position =  last.nil?? 1:last.position+1
+    self.permalink = self.permalink.parameterize
+  end
+
 end
