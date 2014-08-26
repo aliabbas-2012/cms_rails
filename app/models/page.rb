ab8aae976056bfd860dfcbc7cd963c9b6744a2a2
Ali Abbas <itsgeniusstar@gmail.com>
2014-08-26 11:22:40 +0500

    page attributes labels

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,12 @@
 class Page < ActiveRecord::Base
   belongs_to :subject
   validates :name, uniqueness: true
+
+  HUMANIZED_ATTRIBUTES = {
+      :name => 'Name',:subject_id => 'Subject',:position => 'Order',:is_visible=>'Visibility',
+  }
+
+  def self.human_attribute_name(attr)
+    HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+  end
 end
