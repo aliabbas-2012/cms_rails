cd101510be88a1a97cc32f7394ee9923b0d24b95
Ali Abbas <itsgeniusstar@gmail.com>
2014-08-26 11:30:46 +0500

    page attributes labels

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,7 +6,8 @@ class Page < ActiveRecord::Base
       :name => 'Name',:subject_id => 'Subject',:position => 'Order',:is_visible=>'Visibility',
   }
 
-  def self.human_attribute_name(attr)
+  def self.human_attribute_name(attr,options = {})
     HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+    
   end
 end
