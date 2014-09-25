d2a601b8049f36cc9acad5e4eb4794abcda79bac
Ali Abbas <itsgeniusstar@gmail.com>
2014-09-25 15:07:13 +0500

    all code rails pagination

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -46,7 +46,7 @@ class SubjectsController < ApplicationController
 
     respond_to do |format|
       if @subject.save
-        format.html { redirect_to @subject, notice: 'Subject was successfully created.' }
+        format.html { redirect_to @subject, :flash => { :success =>'Subject was successfully created.' }}
         format.json { render :show, status: :created, location: @subject }
       else
         format.html { render :new }
@@ -60,7 +60,7 @@ class SubjectsController < ApplicationController
   def update
     respond_to do |format|
       if @subject.update(subject_params)
-        format.html { redirect_to @subject, notice: 'Subject was successfully updated.' }
+        format.html { redirect_to @subject, :flash => { :success =>'Subject was successfully updated.' }}
         format.json { render :show, status: :ok, location: @subject }
       else
         format.html { render :edit }
@@ -74,7 +74,7 @@ class SubjectsController < ApplicationController
   def destroy
     @subject.destroy
     respond_to do |format|
-      format.html { redirect_to subjects_url, notice: 'Subject was successfully destroyed.' }
+      format.html { redirect_to subjects_url, :flash => { :success => 'Subject was successfully destroyed.' }}
       format.json { head :no_content }
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,19 @@ module ApplicationHelper
       content_tag(:span, options[:false_text], :class => 'status false', :alt => value, :title => value)
     end
   end
+
+  def bootstrap_class_for flash_type
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-error"
+      when :alert
+        "alert-block"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
 end
