90f61552b2615af394da5f5d86a1815ab171aabf
Ali Abbas <itsgeniusstar@gmail.com>
2014-09-26 18:10:47 +0500

    active admin module

---
+++ app/admin/post.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Post do
+
+  permit_params :name, :body,:permalink
+  form do |f|
+    f.inputs "Details" do
+      f.input :name
+      f.input :permalink
+
+    end
+    f.inputs "Content" do
+      f.input :body
+    end
+    f.actions
+  end
+
+end
